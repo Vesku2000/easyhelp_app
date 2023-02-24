6c7ce6298f13602d2f646814d23e290f5c672f3d
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-02-24 13:57:25 +0200

    editing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <h1>Tervetuloa yritykseni verkkosivuille</h1>
+      {/* Tässä voit lisätä muita sivun sisältöjä */}
     </div>
   );
 }
